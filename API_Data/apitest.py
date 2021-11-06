fe62e6a4de4b2594f7a7b3abdca146c649f66d88
Cameron Day <c.day960@gmail.com>
2021-11-05 21:19:37 -0500

    needed values

--- API_Data/apitest.py
+++ API_Data/apitest.py
@@ -1,7 +1,23 @@
 import requests
 import json
-response_API = requests.get("https://api.le-systeme-solaire.net/rest")
+response_API = requests.get("https://api.le-systeme-solaire.net/rest/bodies/")
 data = response_API.text
-json = json.loads(data)
-case = json['bodies']
-print(case)
+body_data = json.loads(data)
+bodies = body_data["bodies"]
+
+localSystem = []
+
+for object in bodies:
+    localSystem.append(object['englishName'])
+
+#print(localSystem)
+
+for planet in bodies:
+    print("Name: {name}, AvgTemp: {temp}, Mass: {mass}, Volume: {volume}, Density: {density}, Gravity: {gravity}".format(
+        name=planet["englishName"],
+        temp=planet["avgTemp"],
+        mass=planet["mass"],
+        volume=planet["vol"],
+        density=planet["density"],
+        gravity=planet["gravity"]
+    ))
