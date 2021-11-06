88f262c8920f29eeaaa9af16d4b7cd826778677f
Ryan Huynh <ryanhuynh@mwc-073048.dhcp.missouri.edu>
2021-11-06 13:27:11 -0500

    added PlanetLocation and calcPosition function

--- API_Data/planetMapping.py
+++ API_Data/planetMapping.py
@@ -1,24 +1,51 @@
-class Planet:
-    def __init__(self,name):
-      self.planetName = name
-      self.mass = 0
-      self.radius = 0
-      self.eccentricity = 0
-      self.orbitalVelocity = 0
+import datetime
+
+class PlanetLocation:
+    def __init__(self, name, period):
+      self.planetName = name #Stored as str
+      self.orbitalPeriod = period #Stored as a float
       
+    #Calculates position of the planet today
+    #Returns current position in degrees
+    def calcPosition(self):
+      start_date = datetime.date(949, 1, 1)
+      end_date = datetime.date.today()
+      delta = end_date - start_date
+      return (delta.days % self.orbitalPeriod) / self.orbitalPeriod * 360
 
 def main():
-  pass
+  #Create variables for planets
+  mercury = PlanetLocation("Mercury",88)
+  venus = PlanetLocation("Venus", 224.7)
+  earth = PlanetLocation("Earth", 365.2)
+  mars = PlanetLocation("Mars", 687)
+  jupiter = PlanetLocation("Jupiter",4331)
+  saturn = PlanetLocation("Saturn",10747)
+  uranus = PlanetLocation("Uranus",30589)
+  neptune = PlanetLocation("Neptune",59800)
+  pluto = PlanetLocation("Pluto",90560)
 
-main()
+  #Create array
+  array_of_planets = [mercury, venus, earth, mars, jupiter, saturn, uranus, neptune, pluto]
 
-#orbitalVelocity is in km/sec
-#Mercury orbitalVelocity = 47.4
-#Venus orbitalVelocity = 35.0
-#Earth orbitalVelocity = 29.8
-#Moon orbitalVelocity = 24.1
-#Jupiter orbitalVelocity = 13.1
-#Saturn orbitalVelocity = 9.7
-#Uranus orbitalVelocity = 6.8
-#Neptune orbitalVelocity = 5.4
-#Pluto orbitalVelocity = 4.7
+  #Print position for all planets
+  for planet in array_of_planets:
+    print(planet.calcPosition())
+  
+if __name__ == "__main__":
+  main()
+
+
+'''the last time all the planets were lined up 
+is 949 ad. The next time is May 6, 2492'''
+#orbitalPeriod is in earth days
+#orbitalPeriod is Earth days it takes to go around the sun
+#Mercury orbitalPeriod = 88.0
+#Venus orbitalPeriod = 224.7
+#Earth orbitalPeriod = 365.2
+#Mars orbitalPeriod = 687.0
+#Jupiter orbitalPeriod = 4331
+#Saturn orbitalPeriod = 10,747
+#Uranus orbitalPeriod = 30,589
+#Neptune orbitalPeriod = 	59,800
+#Pluto orbitalPeriod = 90,560
