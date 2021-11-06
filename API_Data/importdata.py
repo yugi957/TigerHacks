2c2d8999fd80d45475aac3bad338be2532f8d961
Ryan Huynh <ryanhuynh@mwc-073048.dhcp.missouri.edu>
2021-11-06 15:06:08 -0500

    edit to data.json and edit importdata.py

--- API_Data/importdata.py
+++ API_Data/importdata.py
@@ -1,2 +1,45 @@
-# This file imports data form the Ephemeris API
-# https://astrologyapi.docs.apiary.io/#
+# This file imports data from the planet data json file
+
+import requests
+import json
+
+class PlanetInfo:
+  def __init__(self,name,discDate,discBy,radius,massVal,massExp,avgTemp,moons):
+    self.name = name #Stored as str
+    self.discoveryDate = discDate #Stored as a str in dd/mm/yyyy
+    self.discoveredBy = discBy #Stored as str
+    self.radius = radius #Stored as float. Mean radius in km
+    self.massValue = massVal #Stored as float
+    self.massExponent = massExp #Stored as float. Body mass is massValue*10^massExponent
+    self.avgTemp = avgTemp #Stored as float. Mean temp in Kelvin
+    self.moons = moons #Dictionary of strings with names of moons
+
+def main():
+  dataJson = open('data.json')
+  planetData = json.load(dataJson)
+  #print(planetData)
+  localPlanets = ["Earth", "Venus", "Mars", "Mercury", "Saturn", "Neptune","Uranus", "Pluto", "Jupiter"]
+  planets = []
+  
+  for planet in planetData:
+    if planet["name"] in localPlanets:
+      planets.append(PlanetInfo(
+        name = planet["name"],
+        discDate = planet["discoveryDate"],
+        discBy = planet["discoveredBy"],
+        radius = planet["radius"],
+        massVal = planet["mass"]["massValue"],
+        massExp = planet["mass"]["massExponent"],
+        avgTemp = planet["temp"],
+        moons = planet["moons"]
+      ))
+  for planet in planets:
+    print(planet.name)
+
+  dataJson.close()
+
+  
+
+if __name__ == "__main__":
+  main()
+
