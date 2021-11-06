b695914c4164bdd2e0a608cd294673e6e8172daf
Ryan Huynh <ryanhuynh@mwc-074113.dhcp.missouri.edu>
2021-11-06 11:04:31 -0500

    edit planetMapping.py and add planetMapping.json

--- API_Data/planetMapping.py
+++ API_Data/planetMapping.py
@@ -1,2 +1,24 @@
 class Planet:
-    def __init__
+    def __init__(self,name):
+      self.planetName = name
+      self.mass = 0
+      self.radius = 0
+      self.eccentricity = 0
+      self.orbitalVelocity = 0
+      
+
+def main():
+  pass
+
+main()
+
+#orbitalVelocity is in km/sec
+#Mercury orbitalVelocity = 47.4
+#Venus orbitalVelocity = 35.0
+#Earth orbitalVelocity = 29.8
+#Moon orbitalVelocity = 24.1
+#Jupiter orbitalVelocity = 13.1
+#Saturn orbitalVelocity = 9.7
+#Uranus orbitalVelocity = 6.8
+#Neptune orbitalVelocity = 5.4
+#Pluto orbitalVelocity = 4.7
