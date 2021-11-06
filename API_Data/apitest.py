d70fc397d09a7e706e9d298bc69e1026c0dcc9fa
Cameron Day <c.day960@gmail.com>
2021-11-05 21:41:13 -0500

    dictionary of data

--- API_Data/apitest.py
+++ API_Data/apitest.py
@@ -11,13 +11,26 @@ for object in bodies:
     localSystem.append(object['englishName'])
 
 #print(localSystem)
+localSystem = {"name":[], "temp":[], "mass":[], "volume":[], "density":[], "gravity":[]}
+
+data = []
 
 for planet in bodies:
-    print("Name: {name}, AvgTemp: {temp}, Mass: {mass}, Volume: {volume}, Density: {density}, Gravity: {gravity}".format(
+    """print("Name: {name}, AvgTemp: {temp}, Mass: {mass}, Volume: {volume}, Density: {density}, Gravity: {gravity}".format(
         name=planet["englishName"],
         temp=planet["avgTemp"],
         mass=planet["mass"],
         volume=planet["vol"],
         density=planet["density"],
         gravity=planet["gravity"]
-    ))
+    ))"""
+    item = {"name": planet["englishName"], 
+        "temp": planet["avgTemp"],
+        "mass":planet["mass"], 
+        "volume": planet["vol"],
+        "density": planet["density"],
+        "gravity": planet["gravity"]}
+    data.append(item)
+
+
+print(data)
