a92e6e032bbbe8e18eccb8c15fb63a6ed6614e71
Andrew Kim <66102642+ahkim3@users.noreply.github.com>
2021-11-06 22:42:34 -0500

    Add rotations of planets

---
+++ SpaceNav/Assets/scripts/PlanetRotations/RotateMercury.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    //365.26
+    private float revolutionPD = 360 / 87.96f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+        Vector3 point = new Vector3(0, 0, 0);
+        Vector3 axis = new Vector3(0, 0, 1);
+        transform.RotateAround(point, axis, Time.deltaTime * revolutionPD);
+
+
+
+
+    }
+}
