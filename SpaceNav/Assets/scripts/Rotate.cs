22ac8d00f622ab82f2a1a20f54ee4a4af1330644
yugi957 <atul.krishnadas@outlook.com>
2021-11-06 21:02:02 -0500

    Aspect and initial rotate

---
+++ SpaceNav/Assets/scripts/Rotate.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+
+    private float waitTime = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+        Vector3 point = new Vector3(0,0,0);
+        Vector3 axis = new Vector3(0,0,1);
+        transform.RotateAround(point, axis, Time.deltaTime * 35);
+
+    }
+}
