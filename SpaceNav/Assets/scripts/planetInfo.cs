742ccd45fb1e34e605366133b34f6dc3fdc5d287
yugi957 <atul.krishnadas@outlook.com>
2021-11-07 09:55:04 -0600

    Final exe Build

--- SpaceNav/Assets/scripts/planetInfo.cs
+++ SpaceNav/Assets/scripts/planetInfo.cs
@@ -99,12 +99,12 @@ public class planetInfo : MonoBehaviour
                     currentPlanet = planetList[i];
                 }
             }
-            text.text = currentPlanet.name + "\nTemperature: " + currentPlanet.temp + "\nMass: " + Math.Round(currentPlanet.mass.massValue, 2) + "x10^" + currentPlanet.mass.massExponent + "\nGravity: " + currentPlanet.gravity
-            + "\nDiscovered By: StL. Bread Co \n" + "in 1980";
+            text.text = currentPlanet.name + "\nTemperature: " + currentPlanet.temp + "K\nMass: " + Math.Round(currentPlanet.mass.massValue, 2) + "x10^" + currentPlanet.mass.massExponent + "kg\nGravity: " + currentPlanet.gravity
+            + "m/s^2\nDiscovered By: StL. Bread Co \n" + "in 1980";
         }
         else
         {
-            text.text = currentPlanet.name + "\nTemperature: " + currentPlanet.temp + "\nMass: " + Math.Round(currentPlanet.mass.massValue, 2) + "x10^" + currentPlanet.mass.massExponent + "\nGravity: " + currentPlanet.gravity
+            text.text = currentPlanet.name + "\nTemperature: " + currentPlanet.temp + "K\nMass: " + Math.Round(currentPlanet.mass.massValue, 2) + "x10^" + currentPlanet.mass.massExponent + "kg\nGravity: " + currentPlanet.gravity
             + "\nDiscovered By: " + currentPlanet.discoveredBy + "\non " + currentPlanet.discoveryDate;
         }
     }
@@ -149,13 +149,13 @@ public class planetInfo : MonoBehaviour
                     currentPlanet = obj;
                 }
             }
-            text.text = currentPlanet.name + "\nTemperature: " + currentPlanet.temp + "\nMass: " + Math.Round(currentPlanet.mass.massValue, 2) + "x10^" + currentPlanet.mass.massExponent + "\nGravity: " + currentPlanet.gravity
-            + "\nDiscovered By: StL. Bread Co \n" + "in 1980";
+            text.text = currentPlanet.name + "\nTemperature: " + currentPlanet.temp + " K\nMass: " + Math.Round(currentPlanet.mass.massValue, 2) + "x10^" + currentPlanet.mass.massExponent + " kg\nGravity: " + currentPlanet.gravity
+            + " m/s^2\nDiscovered By: StL. Bread Co \n" + "in 1980";
         }
         else
         {
-            text.text = currentPlanet.name + "\nTemperature: " + currentPlanet.temp + "\nMass: " + Math.Round(currentPlanet.mass.massValue, 2) + "x10^" + currentPlanet.mass.massExponent + "\nGravity: " + currentPlanet.gravity
-            + "\nDiscovered By: " + currentPlanet.discoveredBy + "\non " + currentPlanet.discoveryDate;
+            text.text = currentPlanet.name + "\nTemperature: " + currentPlanet.temp + " K\nMass: " + Math.Round(currentPlanet.mass.massValue, 2) + "x10^" + currentPlanet.mass.massExponent + " kg\nGravity: " + currentPlanet.gravity
+            + " m/s^2\nDiscovered By: " + currentPlanet.discoveredBy + "\non " + currentPlanet.discoveryDate;
         }
     }
 }
---
+++ SpaceNav/Assets/scripts/sus.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sus : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 horizontal = new Vector3(10, 0.0f, 0.0f);
+        transform.position = transform.position + horizontal * Time.deltaTime;
+
+
+        //ROTATION LOGIC
+        Vector3 point = GameObject.FindGameObjectWithTag("red").transform.position;
+        Vector3 axis = new Vector3(0,0,1);
+        transform.RotateAround(point, axis, Time.deltaTime * 35);
+    }
+}
